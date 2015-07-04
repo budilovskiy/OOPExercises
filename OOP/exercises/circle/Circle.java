437a916c4b408fcefdff758c4d65edbd57e5c508
Maksim Budilovskiy <maksim.budilovskiy@gmail.com>
2015-07-04 10:45:10 +0300

    Update Circle.java

--- OOP/exercises/circle/Circle.java
+++ OOP/exercises/circle/Circle.java
@@ -121,3 +121,85 @@ System.out.println(c2.toString());  // explicit call
 System.out.println(c2);             // println() calls toString() implicitly, same as above
 System.out.println("Operator '+' invokes toString() too: " + c2);  // '+' invokes toString() too
 */
+
+package oop.excercises.circle;
+
+public class Circle {
+	
+	private double radius;
+	private String color;
+	
+	/**
+	 * Default constructor
+	 */
+	public Circle() {
+		radius = 1.0;
+		color = "red";
+	}
+	
+	/**
+	 * Constructor with given radius
+	 * @param radius
+	 */
+	public Circle(double radius) {
+		this.radius = radius;
+		color = "red";
+	}
+	
+	/**
+	 * Constructor with given radius and color
+	 * @param radius
+	 */
+	public Circle(double radius, String color) {
+		this.radius = radius;
+		this.color = color;
+	}
+
+	/**
+	 * Getter for instance variable radius
+	 * @return radius
+	 */
+	public double getRadius() {
+		return radius;
+	}
+	
+	/**
+	 * Getter for instance variable color
+	 * @return color
+	 */
+	public String getColor() {
+		return color;
+	}
+
+	/**
+	 * Getter for instance area
+	 * @return area of the circle
+	 */
+	public double getArea() {
+		return radius*radius*Math.PI;
+	}
+	
+	/**
+	 * Setter for instance variable radius
+	 * @param radius
+	 */
+	public void setRadius(double radius) {
+		this.radius = radius;
+	}
+	
+	/**
+	 * Setter for instance variable color
+	 * @param color
+	 */
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	@Override
+	public String toString() {
+		return "Circle [radius=" + radius + ", color=" + color + ", area="
+				+ getArea() + "]";
+	}
+	
+	
+}
