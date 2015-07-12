a6d6594018ec7dc3e8010e787520366eeab174e0
Maksim Budilovskiy <maksim.budilovskiy@gmail.com>
2015-07-12 15:52:44 +0300

    Update MyDate.java

--- OOP/exercises/TheMyDateClass/MyDate.java
+++ OOP/exercises/TheMyDateClass/MyDate.java
@@ -85,3 +85,349 @@ package oop.excercises.themydateclass;
  * the dates from 28 Dec 2011 to 2 Mar 2012.
  *
  */
+
+public class MyDate {
+
+	private final static int MIN_YEAR = 1;
+	private final static int MAX_YEAR = 9999;
+
+	private int year;
+	private int month;
+	private int day;
+
+	private final static String[] strMonths = { "Jan", "Feb", "Mar", "Apr",
+			"May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
+
+	private final static String[] strDays = { "Sunday", "Monday", "Tuesday",
+			"Wednesday", "Thursday", "Friday", "Saturday" };
+
+	private final static int[] daysInMonths = { 31, 28, 31, 30, 31, 30, 31, 31,
+			30, 31, 30, 31 };
+
+	/**
+	 * A year is a leap year if it is divisible by 4 but not by 100, or it is
+	 * divisible by 400.
+	 * 
+	 * @param year
+	 * @return true if the given year is a leap year
+	 */
+	public static boolean isLeapYear(int year) {
+		return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+	}
+
+	/**
+	 * isValidDate(int year, int month, int day): returns true if the given
+	 * year, month, and day constitute a valid date. Assume that year is between
+	 * 1 and 9999, month is between 1 (Jan) to 12 (Dec) and day shall be between
+	 * 1 and 28|29|30|31 depending on the month and whether it is a leap year on
+	 * Feb.
+	 * 
+	 * @param date
+	 * @return true if the given year, month, and day constitute a valid date
+	 */
+	public static boolean isValidDate(int year, int month, int day) {
+		if (year < MIN_YEAR || year > MAX_YEAR)
+			return false;
+		if (month < 1 || month > 12)
+			return false;
+		int maxDays = daysInMonths[month - 1]
+				+ ((isLeapYear(year) && month == 2) ? 1 : 0);
+		return (day >= 1 && day <= maxDays);
+	}
+
+	/**
+	 * getDayOfWeek(int year, int month, int day): returns the day of the week,
+	 * where 0 for Sun, 1 for Mon, ..., 6 for Sat, for the given date. Assume
+	 * that the date is valid. Read the earlier exercise
+	 * (https://www3.ntu.edu.sg
+	 * /home/ehchua/programming/java/J2a_BasicsExercises.html#dateutil) on how
+	 * to determine the day of the week (or Wiki
+	 * "Determination of the day of the week").
+	 * 
+	 * @param day
+	 * @param month
+	 * @param year
+	 * @return the day of the week, where 0 for Sun, 1 for Mon, ..., 6 for Sat,
+	 *         for the given date. If date is not valid, returns -1;
+	 */
+	public static int getDayOfWeek(int year, int month, int day) {
+		if (!isValidDate(year, month, day))
+			return -1;
+
+		// 1. Based on the first two digit of the year, get the number from the
+		// "century" table.
+		int magicCenturyNumber = 6 - 2 * ((year / 100) % 4);
+
+		// 2. Add to the last two digit of the year.
+		int lastTwoDigitsOfYear = year % 100;
+
+		// 3. Add to
+		// "the last two digit of the year divide by 4, truncate the fractional part".
+		int magicYearNumber = lastTwoDigitsOfYear / 4;
+
+		// 4. Add to the number obtained from the month table:
+		int[] nonLeapYearMonthNumbers = { 0, 3, 3, 6, 1, 4, 6, 2, 5, 0, 3, 5 };
+		int[] leapYearMonthNumbers = { 6, 2, 3, 6, 1, 4, 6, 2, 5, 0, 3, 5 };
+		int magicMonthNumber = (isLeapYear(year) ? leapYearMonthNumbers[month - 1]
+				: nonLeapYearMonthNumbers[month - 1]);
+
+		// 5. Add to the day
+		int magicDayNumber = day;
+
+		// 6. The sum modulus 7 gives the day of the week, where 0 for SUN, 1
+		// for MON, ..., 6 for SAT.
+		return (magicCenturyNumber + lastTwoDigitsOfYear + magicYearNumber
+				+ magicMonthNumber + magicDayNumber) % 7;
+	}
+
+	/**
+	 * Constructor, which takes 3 parameters: year, month and day. It shall
+	 * invoke setDate() method to set the instance variables.
+	 * 
+	 * @param year
+	 * @param month
+	 * @param day
+	 */
+	public MyDate(int year, int month, int day) {
+		try {
+			setDate(year, month, day);
+		} catch (IllegalArgumentException iae) {
+			System.out.println(iae.getMessage());
+		}
+	}
+
+	/**
+	 * setDate(int year, int month, int day): It shall invoke the static method
+	 * isValidDate() to verify that the given year, month and day constitute a
+	 * valid date. (Advanced: Otherwise, it shall throw an
+	 * IllegalArgumentException with the message
+	 * "Invalid year, month, or day!".)
+	 * 
+	 * @param year
+	 * @param month
+	 * @param day
+	 */
+	public void setDate(int year, int month, int day)
+			throws IllegalArgumentException {
+		if (isValidDate(year, month, day)) {
+			this.year = year;
+			this.month = month;
+			this.day = day;
+		} else {
+			throw new IllegalArgumentException("Invalid year, month, or day!");
+		}
+	}
+
+	/**
+	 * setYear(int year): It shall verify that the given year is between 1 and
+	 * 9999. (Advanced: Otherwise, it shall throw an IllegalArgumentException
+	 * with the message "Invalid year!".)
+	 * 
+	 * @param year
+	 */
+	public void setYear(int year) {
+		if (year >= MIN_YEAR && year <= MAX_YEAR) {
+			this.year = year;
+		} else {
+			throw new IllegalArgumentException("Invalid year!");
+		}
+	}
+
+	/**
+	 * setMonth(int month): It shall verify that the given month is between 1
+	 * and 12. (Advanced: Otherwise, it shall throw an IllegalArgumentException
+	 * with the message "Invalid month!".)
+	 * 
+	 * @param month
+	 */
+	public void setMonth(int month) {
+		if (month >= 1 && month <= 12) {
+			this.month = month;
+		} else {
+			throw new IllegalArgumentException("Invalid month!");
+		}
+	}
+
+	/**
+	 * setDay(int day): It shall verify that the given day is between 1 and
+	 * dayMax, where dayMax depends on the month and whether it is a leap year
+	 * for Feb. (Advanced: Otherwise, it shall throw an IllegalArgumentException
+	 * with the message "Invalid day!".)
+	 * 
+	 * @param day
+	 */
+	public void setDay(int day) {
+		if ((day >= 1 && day <= daysInMonths[month - 1])
+				|| (isLeapYear(year) && month == 2 && day >= 1 && day <= 29)) {
+			this.day = day;
+		} else {
+			throw new IllegalArgumentException("Invalid day!");
+		}
+	}
+
+	//
+	// getYear(), getMonth(), getDay():
+	// return the value for the year, month and day, respectively.
+	//
+	public int getYear() {
+		return year;
+	}
+
+	public int getMonth() {
+		return month;
+	}
+
+	public int getDay() {
+		return day;
+	}
+
+	// toString():
+	// returns a date string in the format "xxxday d mmm yyyy",
+	// e.g., "Tuesday 14 Feb 2012".
+	@Override
+	public String toString() {
+		if (!isValidDate(year, month, day)) {
+			return "Not a valid date!";
+		}
+		return strDays[getDayOfWeek(year, month, day)] + " " + day + " "
+				+ strMonths[month - 1] + " " + year;
+	}
+
+	/**
+	 * nextDay(): update this instance to the next day and return this instance.
+	 * Take note that nextDay() for 31 Dec 2000 shall be 1 Jan 2001.
+	 * 
+	 * @return this instance with increased day
+	 */
+	public MyDate nextDay() throws IllegalStateException {
+		day++;
+		int maxDays = daysInMonths[month - 1]
+				+ ((isLeapYear(year) && month == 2) ? 1 : 0);
+		if (day > maxDays) {
+			day = 1;
+			month++;
+		}
+		if (month > 12) {
+			month = 1;
+			year++;
+		}
+		if (year > MAX_YEAR) {
+			throw new IllegalStateException("Year out of range!");
+		}
+		return this;
+	}
+
+	/**
+	 * nextMonth(): update this instance to the next month and return this
+	 * instance. Take note that nextMonth() for 31 Oct 2012 shall be 30 Nov
+	 * 2012.
+	 * 
+	 * @return this instance with increased month
+	 */
+	public MyDate nextMonth() throws IllegalStateException {
+		month++;
+		if (month > 12) {
+			month = 1;
+			year++;
+		}
+		if (year > MAX_YEAR) {
+			throw new IllegalStateException("Year out of range!");
+		}
+		int maxDays = daysInMonths[month - 1]
+				+ ((isLeapYear(year) && month == 2) ? 1 : 0);
+		if (day > maxDays) {
+			day = maxDays;
+		}
+		return this;
+	}
+
+	/**
+	 * nextYear(): update this instance to the next year and return this
+	 * instance. Take note that nextYear() for 29 Feb 2012 shall be 28 Feb 2013.
+	 * (Advanced: throw an IllegalStateException with the message
+	 * "Year out of range!" if year > 9999.)
+	 * 
+	 * @return this instance with increased year
+	 */
+	public MyDate nextYear() throws IllegalStateException {
+		year++;
+		if (year > MAX_YEAR) {
+			throw new IllegalStateException("Year out of range!");
+		}
+		int maxDays = daysInMonths[month - 1]
+				+ ((isLeapYear(year) && month == 2) ? 1 : 0);
+		if (day > maxDays) {
+			day = maxDays;
+		}
+		return this;
+	}
+
+	/**
+	 * previousDay(): update this instance to the previous day and return this
+	 * instance.
+	 * 
+	 * @return this instance with decreased day
+	 */
+	public MyDate previousDay() throws IllegalStateException {
+		day--;
+		if (day < 1) {
+			day = 31;
+			month--;
+		}
+		if (month < 1) {
+			month = 12;
+			year--;
+		}
+		if (year < MIN_YEAR) {
+			throw new IllegalStateException("Year out of range!");
+		}
+		int maxDays = daysInMonths[month - 1]
+				+ ((isLeapYear(year) && month == 2) ? 1 : 0);
+		if (day > maxDays) {
+			day = maxDays;
+		}
+		return this;
+	}
+
+	/**
+	 * previousMonth(): update this instance to the previous month and return
+	 * this instance.
+	 * 
+	 * @return this instance with decreased month
+	 */
+	public MyDate previousMonth() throws IllegalStateException {
+		month--;
+		if (month < 1) {
+			month = 12;
+			year--;
+		}
+		if (year < MIN_YEAR) {
+			throw new IllegalStateException("Year out of range!");
+		}
+		int maxDays = daysInMonths[month - 1]
+				+ ((isLeapYear(year) && month == 2) ? 1 : 0);
+		if (day > maxDays) {
+			day = maxDays;
+		}
+		return this;
+	}
+
+	/**
+	 * previousYear(): update this instance to the previous year and return this
+	 * instance.
+	 * 
+	 * @return this instance with decreased year
+	 */
+	public MyDate previousYear() throws IllegalStateException {
+		year--;
+		if (year < MIN_YEAR) {
+			throw new IllegalStateException("Year out of range!");
+		}
+		int maxDays = daysInMonths[month - 1]
+				+ ((isLeapYear(year) && month == 2) ? 1 : 0);
+		if (day > maxDays) {
+			day = maxDays;
+		}
+		return this;
+	}
+}
