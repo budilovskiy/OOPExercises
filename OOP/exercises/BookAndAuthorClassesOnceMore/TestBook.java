0c5a93201fd1347e21d1dcf57377a5714eb5f38a
Maksim Budilovskiy <maksim.budilovskiy@gmail.com>
2015-07-13 21:29:30 +0300

    Create TestBook.java

---
+++ OOP/exercises/BookAndAuthorClassesOnceMore/TestBook.java
@@ -0,0 +1,24 @@
+package oop.excercises.bookbndbuthorclassesoncemore;
+
+public class TestBook {
+
+	public static void main(String[] args) {
+			 
+		Book javaDummy = new Book("Java for Dummy", 19.99, 99);
+		System.out.println(javaDummy);  // toString()
+		System.out.print("The authors are: ");
+		javaDummy.printAuthors();
+		 
+		javaDummy.addAuthor(new Author("Tan Ah Teck", "AhTeck@somewhere.com", 'm'));
+		javaDummy.addAuthor(new Author("Paul Tan", "Paul@nowhere.com", 'm'));
+		System.out.println(javaDummy);  // toString()
+		System.out.print("The authors are: ");
+		javaDummy.printAuthors();
+		
+		System.out.println("Removing author Tan Ah Teck");
+		javaDummy.removeAuthorByName("Tan Ah Teck");
+		System.out.print("The authors are: ");
+		javaDummy.printAuthors();
+
+	}
+}
