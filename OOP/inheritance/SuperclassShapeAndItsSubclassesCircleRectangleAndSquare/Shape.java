8d18ebced623fea713d741e7a77f6f3fd961638d
Maksim Budilovskiy <maksim.budilovskiy@gmail.com>
2015-07-15 15:06:37 +0300

    Update Shape.java

--- OOP/inheritance/SuperclassShapeAndItsSubclassesCircleRectangleAndSquare/Shape.java
+++ OOP/inheritance/SuperclassShapeAndItsSubclassesCircleRectangleAndSquare/Shape.java
@@ -12,6 +12,41 @@ package oop.excersizes.superclassshapeanditssubclassescirclerectangleandsquare
  * A toString() method that returns "A Shape with color of xxx and filled/Not filled".
  * Write a test program to test all the methods defined in Shape. 
  * 
+ * 
+ * Write two subclasses of Shape called Circle and Rectangle,.
+ * 
+ * The Circle class contains:
+ * - An instance variable radius (double).
+ * - Three constructors as shown. The no-arg constructor initializes the radius to 1.0.
+ * - Getter and setter for the instance variable radius.
+ * - Methods getArea() and getPerimeter().
+ * - Override the toString() method inherited, to return "A Circle with radius=xxx, 
+ *    which is a subclass of yyy", where yyy is the output of the toString() method 
+ *    from the superclass.
+ * 
+ * The Rectangle class contains:
+ * - Two instance variables width (double) and length (double).
+ * - Three constructors as shown. The no-arg constructor initializes the width and length to 1.0.
+ * - Getter and setter for all the instance variables.
+ * - Methods getArea() and getPerimeter().
+ * - Override the toString() method inherited, to return "A Rectangle with width=xxx and length=zzz, 
+ *    which is a subclass of yyy", where yyy is the output of the toString() method from the 
+ *    superclass.
+ * 
+ * Write a class called Square, as a subclass of Rectangle. Convince yourself that Square can be 
+ * modeled as a subclass of Rectangle. Square has no instance variable, but inherits the instance 
+ * variables width and length from its superclass Rectangle.
+ * Provide the appropriate constructors (as shown in the class diagram). Hint:
+ * 
+ * public Square(double side) {
+ *  super(side, side);  // Call superclass Rectangle(double, double)
+ * }
+ * Override the toString() method to return "A Square with side=xxx, which is a subclass of yyy", 
+ * where yyy is the output of the toString() method from the superclass.
+ * Do you need to override the getArea() and getPerimeter()? Try them out.
+ * Override the setLength() and setWidth() to change both the width and length, so as to maintain 
+ * the square geometry.
+ * 
  */
 
 public class Shape {
