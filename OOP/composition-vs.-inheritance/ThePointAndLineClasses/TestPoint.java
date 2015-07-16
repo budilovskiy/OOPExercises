1908466679759fef7c1eb83235a2b61cf0a4a4fb
Maksim Budilovskiy <maksim.budilovskiy@gmail.com>
2015-07-16 19:43:11 +0300

    Create TestPoint.java

---
+++ OOP/composition-vs.-inheritance/ThePointAndLineClasses/TestPoint.java
@@ -0,0 +1,17 @@
+package oop.excercises.thepointandlineclasses;
+
+public class TestPoint {
+
+	public static void main(String... args) {
+
+		Point p1 = new Point(10, 20); // Construct a Point
+		System.out.println(p1);
+		
+		p1.setXY(100, 10);
+		System.out.println(p1);
+		
+		p1.setX(150);
+		p1.setY(0);
+		System.out.println(p1);
+	}
+}
