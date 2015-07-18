e905b161377df69a66a1fadb1bf72b7c062012f9
Maksim Budilovskiy <maksim.budilovskiy@gmail.com>
2015-07-18 17:31:18 +0300

    Create BigDog.java

---
+++ OOP/Polymorphism-Abstract-Classes-and-Interfaces/Polymorphism/BigDog.java
@@ -0,0 +1,15 @@
+package oop.excercises.polymorphism;
+
+public class BigDog extends Dog {
+	
+	@Override
+	public void greeting() {
+		System.out.println("Woow!");
+	}
+	
+	@Override
+	public void greeting(Dog another) {
+		System.out.println("Woooooowwwww!");
+	}
+
+}
