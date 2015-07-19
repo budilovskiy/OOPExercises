3809a5b645d13b57ba98c359a63c0e215fa97108
Maksim Budilovskiy <maksim.budilovskiy@gmail.com>
2015-07-19 12:46:57 +0300

    Update TestDiscountSystem.java

--- OOP/More-Exercises-on-OOP/TheDiscountSystem/TestDiscountSystem.java
+++ OOP/More-Exercises-on-OOP/TheDiscountSystem/TestDiscountSystem.java
@@ -20,16 +20,16 @@ public class TestDiscountSystem {
 		visit2.setServiceExpense(100);
 		visit2.setProductExpense(100);
 		System.out.println(visit2);
+		customer1.setMember(true);
 		
 		System.out.println("----------------");
-		customer1.setMember(true);
 		Visit visit3 = new Visit(customer1, new Date());
 		visit3.setServiceExpense(100);
 		visit3.setProductExpense(100);
 		System.out.println(visit3);
+		customer1.setMemberType("Gold");
 		
 		System.out.println("----------------");
-		customer1.setMemberType("Gold");
 		Visit visit4 = new Visit(customer1, new Date());
 		visit4.setServiceExpense(100);
 		visit4.setProductExpense(100);
