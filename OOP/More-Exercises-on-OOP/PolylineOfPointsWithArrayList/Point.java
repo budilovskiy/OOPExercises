9135c9e845d14bf9fae1ff760cac6ef8530a9edf
Maksim Budilovskiy <maksim.budilovskiy@gmail.com>
2015-07-19 14:40:40 +0300

    Update Point.java

--- OOP/More-Exercises-on-OOP/PolylineOfPointsWithArrayList/Point.java
+++ OOP/More-Exercises-on-OOP/PolylineOfPointsWithArrayList/Point.java
@@ -1,1 +1,24 @@
+/*
+ * A polyline is a line with segments formed by points. Let's use the 
+ * ArrayList (dynamically allocated array) to keep the points, but 
+ * upcast to List in the instance variable. (Take note that array 
+ * is of fixed-length, and you need to set the initial length).
+ */
+package oop.excercises.polylineofpointswitharrayList;
 
+public class Point {
+	
+	private int x;
+	private int y;
+	
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	@Override
+	public String toString() {
+		return "(" + x + ", " + y + ")";
+	}
+
+}
