d5b71782d0fb6ce65bec1bd9d24270a07263fb88
Maksim Budilovskiy <maksim.budilovskiy@gmail.com>
2015-07-19 11:09:42 +0300

    Create GeometricObject.java

---
+++ OOP/Polymorphism-Abstract-Classes-and-Interfaces/InterfacesGeometricObjectAndResizable/GeometricObject.java
@@ -0,0 +1,12 @@
+/*
+ * Write the interface called GeometricObject, which declares 
+ * two abstract methods: getParameter() and getArea()
+ */
+package oop.excercises.interfacesgeometricobjectandresizable;
+
+public interface GeometricObject {
+	
+	public double getPerimeter();
+	public double getArea();
+
+}
